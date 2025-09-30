107c6e9d83b55b99b4a05b63d3fe77284d41152a
Ameni Refai <refaiameni8@gmail.com>
2025-09-30 20:49:08 +0100

    Manipulation du tableau d'authors: associatif, filtre et Structure conditionnelle.

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -4,15 +4,73 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class AuthorController extends AbstractController
+class AuthorController extends AbstractController
 {
-   #[Route('/author/{name}', name: 'show_author')]
-    public function showAuthor(string $name): Response
+    #[Route('/authors', name: 'list_authors')]
+    public function listAuthors(): Response
     {
-        return $this->render('author/show.html.twig', [
-            'name' => $name,
-            ]);
+        $authors = array(
+            array(
+                'id' => 1,
+                'picture' => '/images/Victor-Hugo.jpg',
+                'username' => 'Victor Hugo',
+                'email' => 'victor.hugo@gmail.com',
+                'nb_books' => 100
+            ),
+            array(
+                'id' => 2,
+                'picture' => '/images/william-shakespeare.jpg',
+                'username' => 'William Shakespeare',
+                'email' => 'william.shakespeare@gmail.com',
+                'nb_books' => 200
+            ),
+            array(
+                'id' => 3,
+                'picture' => '/images/Taha_Hussein.jpg',
+                'username' => 'Taha Hussein',
+                'email' => 'taha.hussein@gmail.com',
+                'nb_books' => 300
+            ),
+        );
+
+        return $this->render('author/list.html.twig', [
+            'authors' => $authors
+        ]);
+    }
+
+    #[Route('/author/{id}', name: 'author_details')]
+    public function authorDetails(int $id): Response
+    {
+        $authors = array(
+            1 => array(
+                'id' => 1,
+                'picture' => '/images/Victor-Hugo.jpg',
+                'username' => 'Victor Hugo',
+                'email' => 'victor.hugo@gmail.com',
+                'nb_books' => 100
+            ),
+            2 => array(
+                'id' => 2,
+                'picture' => '/images/william-shakespeare.jpg',
+                'username' => 'William Shakespeare',
+                'email' => 'william.shakespeare@gmail.com',
+                'nb_books' => 200
+            ),
+            3 => array(
+                'id' => 3,
+                'picture' => '/images/Taha_Hussein.jpg',
+                'username' => 'Taha Hussein',
+                'email' => 'taha.hussein@gmail.com',
+                'nb_books' => 300
+            ),
+        );
+
+        $author = $authors[$id] ?? null;
+
+        return $this->render('author/showAuthor.html.twig', [
+            'author' => $author
+        ]);
     }
 }
